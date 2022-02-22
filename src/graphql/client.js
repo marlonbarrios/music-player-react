e03bf911da3c39384782bbe23aa07964fe6419dc
marlonbarrios <marlon@dance-tech.net>
2022-02-21 22:05:10 -0500

    react app music player from bootcamp

---
+++ src/graphql/client.js
@@ -0,0 +1,83 @@
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { gql } from "apollo-boost";
+import { GET_QUEUED_SONGS } from "./queries";
+// import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  link: new WebSocketLink({
+    uri: "wss://peaceful-grub-88.hasura.app/v1/graphql",
+    options: {
+      reconnect: true,
+      connectionParams: {
+        headers: {
+          'x-hasura-admin-secret': 'EWul6PtKSIuziWNSr3ejZ6rITLRGCKSK7KLMGTWAOQeneUvSn1B9lKafe0uRR7c1'
+        }
+      }
+    }
+  }),
+  
+  cache: new InMemoryCache(),
+  typeDefs: gql`
+    type Song {
+      id: uuid!
+      title: String!
+      artist: String!
+      thumbnail: String!
+      duration: Float!
+      url: String!
+    }
+
+    input SongInput {
+      id: uuid!
+      title: String!
+      artist: String!
+      thumbnail: String!
+      duration: Float!
+      url: String!
+    }
+
+    type Query {
+      queue: [Song]!
+    }
+
+    type Mutation {
+      addOrRemoveFromQueue(input: SongInput!): [Song]!
+    }
+  `,
+  resolvers: {
+    Mutation: {
+      addOrRemoveFromQueue: (_, { input }, { cache }) => {
+        const queryResult = cache.readQuery({
+          query: GET_QUEUED_SONGS
+        });
+        if (queryResult) {
+          const { queue } = queryResult;
+          const isInQueue = queue.some(song => song.id === input.id);
+          const newQueue = isInQueue
+            ? queue.filter(song => song.id !== input.id)
+            : [...queue, input];
+          cache.writeQuery({
+            query: GET_QUEUED_SONGS,
+            data: { queue: newQueue }
+          });
+          return newQueue;
+        }
+        return [];
+      }
+    }
+  }
+});
+
+const hasQueue = Boolean(localStorage.getItem("queue"));
+
+const data = {
+  queue: hasQueue ? JSON.parse(localStorage.getItem("queue")) : []
+};
+// const client = new ApolloClient({
+//   uri: "https://apollo-music-share.herokuapp.com/v1/graphql"
+// });
+client.writeData({ data });
+
+export default client;
